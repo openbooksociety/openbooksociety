414bcd9a812006ce71d0de6b674191e7572b350a
David Wahrenburg <mail@davidwahrenburg.de>
2023-04-20 17:36:38 +0200

    inlcude json fetch

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-fetch("content-en.json")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+let LANG = "de";
+let contentDE = fetchData("de");
+let contentEN = fetchData("en");
 
+populate();
 
+let langButton = document.querySelector(".language-toggle");
+document.addEventListener("click", toggleLanguage);
 
+async function fetchData(lang) {
+  let data = await fetch("content-" + lang + ".json");
+  let json = await data.json();
+  return json.res;
+}
+
+function toggleLanguage() {
+  LANG = LANG === "de" ? "en" : "de";
+  LANG === "de" ? populate(contentEN) : populate(contentDE);
+  console.log(contentDE);
+}
+
+async function populate(data) {
+  let contentElement = document.querySelector("#text-content");
+  contentElement.innerHTML = data.content;
+}
