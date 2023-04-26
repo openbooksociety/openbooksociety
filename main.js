5a6327fef86b44ff782e38a0626aed5126ba9c70
David Wahrenburg <mail@davidwahrenburg.de>
2023-04-26 21:55:34 +0200

    typos + refactor

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 let LANG = "en";
 let PAGE = "";
+
 let r = document.querySelector(":root");
 
-setRandomCSSColors(3);
-populateNav();
-populateContent();
-// populateGlyphCanvas();
+window.addEventListener("load", () => {
+  populateNav();
+  setRandomCSSColors(3);
+  populateContent();
+});
 
 let menu = document.querySelector(".header.menu");
-let navLinks = menu.querySelectorAll(".menu .nav-link");
+if (!isTouchDevice()) {
+  menu.addEventListener("mouseenter", unstackMenu);
+  menu.addEventListener("mouseleave", stackMenu);
+}
+document.addEventListener("scroll", stackMenu);
 
+let navLinks = menu.querySelectorAll(".menu .nav-link");
 navLinks.forEach((elem) => {
   elem.addEventListener("click", (e) => {
     menu.classList.contains("stacked") ? unstackMenu() : togglePages(e);
@@ -21,21 +28,18 @@ subNavLinks.forEach((elem) => {
   elem.addEventListener("click", togglePages);
 });
 
-if (!isTouchDevice()) {
-  menu.addEventListener("mouseenter", unstackMenu);
-  menu.addEventListener("mouseleave", stackMenu);
-}
-document.addEventListener("scroll", stackMenu);
-
 let title = document.querySelector(".header.title");
 title.addEventListener("click", togglePages);
 
 let langButton = document.querySelector("#language-toggle");
 langButton.addEventListener("click", toggleLanguage);
 
+// ------ functions ------
+
 async function fetchData() {
   let data = await fetch("content-" + LANG + ".json");
   let json = await data.json();
+
   return json;
 }
 
@@ -47,8 +51,10 @@ function toggleLanguage() {
 
 function togglePages(e) {
   if (!e.target.classList.contains("link")) return;
+
   PAGE = e.target.getAttribute("data-href");
   console.log(PAGE);
+
   navLinks.forEach((elem, i) => {
     if (elem == e.target)
       r.style.setProperty(
@@ -57,9 +63,9 @@ function togglePages(e) {
       );
     elem.classList.remove("active");
   });
+
   e.target.classList.add("active");
   populateContent();
-  // populateGlyphCanvas();
 }
 
 function unstackMenu() {
@@ -72,7 +78,6 @@ function stackMenu() {
 
 async function populateNav() {
   let data = await fetchData();
-
   setContentofID("#header-text-container", data.general.header);
   setContentofID("#members-link", data.nav.members);
   setContentofID("#join-link", data.nav.joinOrDonate);
@@ -86,45 +91,19 @@ async function populateContent() {
   let data = await fetchData();
   setContentofID("#text-content", data.content[PAGE]);
   document.querySelector("#text-content").classList = PAGE;
-
 }
 
 function setContentofID(id, content) {
   document.querySelector(id).innerHTML = content != undefined ? content : "";
 }
 
-function populateGlyphCanvas() {
-  let glyphCanvas = document.querySelector("#glyph-canvas");
-  glyphCanvas.innerHTML = "";
-
-  for (let i = 0; i < 3; i++) {
-    let elem = document.createElement("div");
-    elem.classList.add("random-glyph");
-    elem.innerHTML = getRandomGlyph();
-    elem.style.left = Math.floor(Math.random() * window.innerWidth) - 30 + "px";
-    elem.style.top =
-      Math.floor(
-        Math.random() * (window.innerHeight - 0.2 * window.innerWidth) +
-          0.2 * window.innerWidth -
-          30
-      ) + "px";
-
-    glyphCanvas.appendChild(elem);
+function setRandomCSSColors(amount) {
+  for (let i = 0; i < amount; i++) {
+    r.style.setProperty("--" + i, get_random_color());
   }
 }
 
-function getRandomGlyph() {
-  let n = 1;
-  while (
-    (n <= 33 || n >= 47) &&
-    (n <= 58 || n >= 64) &&
-    (n <= 91 || n >= 96) &&
-    (n <= 123 || n >= 126)
-  ) {
-    n = Math.floor(Math.random() * 126);
-  }
-  return String.fromCharCode(n);
-}
+// ------ helpers ------
 
 function isTouchDevice() {
   return (
@@ -132,12 +111,6 @@ function isTouchDevice() {
     navigator.maxTouchPoints > 0 ||
     navigator.msMaxTouchPoints > 0
   );
-}
-
-function setRandomCSSColors(amount) {
-  for (let i = 0; i < amount; i++) {
-    r.style.setProperty("--" + i, get_random_color());
-  }
 }
 
 function rand(min, max) {
@@ -148,5 +121,6 @@ function get_random_color() {
   let h = Math.floor(rand(0, 360));
   let s = Math.floor(rand(50, 100));
   let l = Math.floor(rand(30, 70));
+
   return "hsl(" + h + "deg," + s + "%," + l + "%)";
 }
