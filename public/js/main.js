2121832bcb148bfdf9b85e6e504dad50f9ed7f9d
David Wahrenburg <mail@davidwahrenburg.de>
2023-08-15 00:10:56 +0200

    mobile Seitenmechanik überarbeitet

--- public/js/main.js
+++ public/js/main.js
@@ -4,35 +4,46 @@ let PAGE = "";
 let r = document.querySelector(":root");
 
 window.addEventListener("load", () => {
-  PAGE = window.location.hash.replaceAll("#", "");
-  console.log(PAGE);
-  if (PAGE != "")
-    document
-      .querySelector("[data-href='" + PAGE + "']")
-      .classList.add("active");
+    PAGE = window.location.hash.replaceAll("#", "");
+    console.log(PAGE);
+    if (PAGE != "") {
+        document.querySelector("body").className = PAGE;
+        document
+            .querySelector("[data-href='" + PAGE + "']")
+            .classList.add("active");
+    }
 
-  populateNav();
-  setRandomCSSColors(3);
-  populateContent();
+    populateNav();
+    setRandomCSSColors(3);
+    populateContent();
 });
 
 let menu = document.querySelector(".header.menu");
 if (!isTouchDevice()) {
-  menu.addEventListener("mouseenter", unstackMenu);
-  menu.addEventListener("mouseleave", stackMenu);
+    menu.addEventListener("mouseenter", unstackMenu);
+    menu.addEventListener("mouseleave", stackMenu);
+} else {
+    unstackMenu()
 }
-document.addEventListener("scroll", stackMenu);
+document.addEventListener("scroll", (e) => {
+    console.log(window.scrollY)
+    if (window.scrollY < 10) {
+        unstackMenu()
+    } else {
+        stackMenu()
+    }
+});
 
 let navLinks = menu.querySelectorAll(".menu .nav-link");
 navLinks.forEach((elem) => {
-  elem.addEventListener("click", (e) => {
-    menu.classList.contains("stacked") ? unstackMenu() : togglePages(e);
-  });
+    elem.addEventListener("click", (e) => {
+        menu.classList.contains("stacked") ? unstackMenu() : togglePages(e);
+    });
 });
 
 let subNavLinks = document.querySelectorAll(".imprint-container .nav-link");
 subNavLinks.forEach((elem) => {
-  elem.addEventListener("click", togglePages);
+    elem.addEventListener("click", togglePages);
 });
 
 let title = document.querySelector(".header.title");
@@ -43,106 +54,110 @@ langButton.addEventListener("click", toggleLanguage);
 
 // ------ functions ------
 
-async function fetchData() {
-  let data = await fetch("snippets/content-" + LANG + ".json");
-  let json = await data.json();
+async function fetchSnippetData() {
+    let data = await fetch("snippets/content-" + LANG + ".json");
+    let json = await data.json();
 
-  return json;
+    return json;
 }
 
 async function fetchMDData(slug) {
-  if (slug == "") return "";
-  let data = await fetch(
-    "https://raw.githubusercontent.com/schnavy/openbooksociety/main/pages/" +
-      slug +
-      "-" +
-      LANG +
-      ".md"
-  );
-  let text = await data.text();
-  let html = MarkdownToHtml.parse(text);
-  html = html.replaceAll(/\\/g, "<br/>");
-  return html;
+    if (slug == "") return "";
+    let data = await fetch(
+        "https://raw.githubusercontent.com/schnavy/openbooksociety/main/pages/" +
+        slug +
+        "-" +
+        LANG +
+        ".md"
+    );
+    let text = await data.text();
+    let html = MarkdownToHtml.parse(text);
+    html = html.replaceAll(/\\/g, "<br/>");
+    return html;
 }
 
 function toggleLanguage() {
-  LANG = LANG === "de" ? "en" : "de";
-  populateNav();
-  populateContent();
+    LANG = LANG === "de" ? "en" : "de";
+    populateNav();
+    populateContent();
 }
 
 function togglePages(e) {
-  if (!e.target.classList.contains("link")) return;
+    if (!e.target.classList.contains("link")) return;
 
-  PAGE = e.target.getAttribute("data-href");
-  console.log(PAGE);
+    PAGE = e.target.getAttribute("data-href");
+    console.log(PAGE);
 
-  navLinks.forEach((elem, i) => {
-    if (elem == e.target)
-      r.style.setProperty(
-        "--link-color",
-        getComputedStyle(r).getPropertyValue("--" + i)
-      );
-    elem.classList.remove("active");
-  });
+    navLinks.forEach((elem, i) => {
+        if (elem === e.target)
+            r.style.setProperty(
+                "--link-color",
+                getComputedStyle(r).getPropertyValue("--" + i)
+            );
+        elem.classList.remove("active");
+    });
 
-  e.target.classList.add("active");
-  populateContent();
+    document.querySelector("body").className = PAGE;
+
+    e.target.classList.add("active");
+    populateContent();
+    window.scrollTo(0, 0)
 }
 
 function unstackMenu() {
-  menu.classList.remove("stacked");
+    menu.classList.remove("stacked");
 }
 
 function stackMenu() {
-  menu.classList.add("stacked");
+    menu.classList.add("stacked");
 }
 
 async function populateNav() {
-  let data = await fetchData();
-  setContentofID("#header-text-container", data.general.header);
-  setContentofID("#members-link", data.nav.members);
-  setContentofID("#join-link", data.nav.joinOrDonate);
-  setContentofID("#language-toggle", data.general.navLanguage);
-  setContentofID("#about-link", data.nav.about);
-  setContentofID("#imprint-link", data.nav.imprint);
-  setContentofID("#privacy-link", data.nav.privacy);
+    let data = await fetchSnippetData();
+    setContentofID("#header-text-container", data.general.header);
+    setContentofID("#header-text-container-mobile", data.general.header);
+    setContentofID("#members-link", data.nav.members);
+    setContentofID("#join-link", data.nav.joinOrDonate);
+    setContentofID("#language-toggle", data.general.navLanguage);
+    setContentofID("#about-link", data.nav.about);
+    setContentofID("#imprint-link", data.nav.imprint);
+    setContentofID("#privacy-link", data.nav.privacy);
 }
 
 async function populateContent() {
-  let data = await fetchMDData(PAGE);
-  setContentofID("#text-content", data);
-  document.querySelector("#text-content").classList = PAGE;
+    let data = await fetchMDData(PAGE);
+    setContentofID("#text-content", data);
+    document.querySelector("#text-content").classList = PAGE;
 }
 
 function setContentofID(id, content) {
-  document.querySelector(id).innerHTML = content != undefined ? content : "";
+    document.querySelector(id).innerHTML = content != undefined ? content : "";
 }
 
 function setRandomCSSColors(amount) {
-  for (let i = 0; i < amount; i++) {
-    r.style.setProperty("--" + i, get_random_color());
-  }
+    for (let i = 0; i < amount; i++) {
+        r.style.setProperty("--" + i, get_random_color());
+    }
 }
 
 // ------ helpers ------
 
 function isTouchDevice() {
-  return (
-    "ontouchstart" in window ||
-    navigator.maxTouchPoints > 0 ||
-    navigator.msMaxTouchPoints > 0
-  );
+    return (
+        "ontouchstart" in window ||
+        navigator.maxTouchPoints > 0 ||
+        navigator.msMaxTouchPoints > 0
+    );
 }
 
 function rand(min, max) {
-  return min + Math.random() * (max - min);
+    return min + Math.random() * (max - min);
 }
 
 function get_random_color() {
-  let h = Math.floor(rand(0, 360));
-  let s = Math.floor(rand(50, 100));
-  let l = Math.floor(rand(30, 70));
+    let h = Math.floor(rand(0, 360));
+    let s = Math.floor(rand(50, 100));
+    let l = Math.floor(rand(30, 70));
 
-  return "hsl(" + h + "deg," + s + "%," + l + "%)";
+    return "hsl(" + h + "deg," + s + "%," + l + "%)";
 }
